e48b53f96f46d41f9f17ce552cf0a94a9e944ab5
Franklin Ramirez <jw937078@gmail.com>
2023-11-29 17:11:27 -0600

    11-29-23d

---
+++ shoeyStore/Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using shoeyStore.Models;
+using shoeyStore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shoeyStore.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(ClientViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            using (var db = new ShoeyDatabaseEntities())
+            {
+                    Cliente clientTO = new Cliente();
+                    clientTO.NombreCliente = model.NombreCliente;
+                    clientTO.NumeroTelefono = model.NumeroTelefono;
+                    clientTO.CorreoElectronico = model.CorreoElectronico;
+                    clientTO.Contrasenna = model.Contrasenna;
+
+                    db.Clientes.Add(clientTO);
+                    db.SaveChanges();
+
+                    return Redirect(Url.Content("~/Home/Index#loginModal"));
+            }
+        }
+    }
+}
