c9e158c6e4c02ac75311e5d1073b1d9448e9eb03
Franklin Ramirez <jw937078@gmail.com>
2023-11-28 22:23:59 -0600

    11-28-23

---
+++ shoeyStore/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using shoeyStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shoeyStore.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string Email, string Password)
+        {
+            try
+            {
+                using (var db = new ShoeyDatabaseEntities())
+                {
+                    var list = from user in db.Clientes
+                              where user.CorreoElectronico == Email && user.Contrasenna == Password
+                              select user;
+
+                    if (list.Count() > 0)
+                    {
+                        Cliente user = list.First();
+                        Session["Logged"] = user;
+                        return Content("200");
+                    }
+                    else
+                    {
+                        return Content("Invalid User");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Content("The following error has occurred: " + ex.Message);
+            }
+
+
+        }
+
+        public ActionResult Logout()
+        {
+            Session["Logged"] = null;
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ shoeyStore/Models/ViewModels/ClientViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace shoeyStore.Models.ViewModels
+{
+    public class ClientViewModel
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public ClientViewModel()
+        {
+            this.Ordens = new HashSet<Orden>();
+        }
+
+        public int IDCliente { get; set; }
+        public string NombreCliente { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Contrasenna { get; set; }
+        public string NumeroTelefono { get; set; }
+        public string Direccion { get; set; }
+        public string TarjetaBancaria { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Orden> Ordens { get; set; }
+    }
+}
